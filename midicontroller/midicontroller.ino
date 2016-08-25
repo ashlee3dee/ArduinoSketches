b82ad1db02d3130385bd51edfc6ba27eff026518
Nicholas Alaniz <alaniz.nicholas@gmail.com>
2016-08-25 15:16:38 -0500

    added button support
    still needs tweaking. when button on pin 3 is pressed, button on pin 2
    is also activated. at this point i am not sure if it is a software or
    hardwaree bug.

--- midicontroller/midicontroller.ino
+++ midicontroller/midicontroller.ino
@@ -1,38 +1,60 @@
-#include <MIDI.h>
+#define CC 176
+#define POT1 A0
+#define MSDELAY 1
+//constants
+const int buttonPin[5]  = {2,   3,  4,  5,  6};
+const int buttonNote[5] = {60,  69, 64, 65, 67};
+//variables
+int val = 0; //Our initial pot values. We need one for the first value and a second to test if there has been a change made. This needs to be done for all 3 pots.
+int lastVal = 0;
+bool currentButtonState[5]  = {LOW, LOW,  LOW,  LOW,  LOW};
+bool lastButtonState[5]     = {LOW, LOW,  LOW,  LOW,  LOW};
 
-MIDI_CREATE_DEFAULT_INSTANCE();
 
-#define LEDPIN 13
-
-const int buttons[] = {2, 3, 4, 5, 6};
-const int notes[] = {50, 51, 52, 53, 54};
-void setup() {
-  // put your setup code here, to run once:
-  MIDI.begin(1);
-  Serial.begin(115200);
-
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
-  pinMode(6, INPUT);
-
-  pinMode(LEDPIN, OUTPUT);
+void setup()
+{
+  Serial.begin(115200);       // Set the speed of the midi port to the same as we will be using in the Hairless Midi software
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  for (int i = 0; i < 4; i++)
+void loop()
+{
+  handleButtons();
+  handlePots();
+  delay(MSDELAY);
+}
+
+void MIDImessage(byte command, byte data1, byte data2) //pass values out through standard Midi Command
+{
+  Serial.write(command);
+  Serial.write(data1);
+  Serial.write(data2);
+}
+
+void handlePots()
+{
+  val = analogRead(A0) / 8; // Divide by 8 to get range of 0-127 for midi
+  if (val != lastVal) // If the value does not = the last value the following command is made. This is because the pot has been turned. Otherwise the pot remains the same and no midi message is output.
   {
-    int buttonState = digitalRead(buttons[i]);
-    int currentNote = notes[i];
-    if (buttonState == HIGH)
+    MIDImessage(CC, 85, val); // 176 = CC command (channel 1 control change), 1 = Which Control, val = value read from Potentionmeter 1
+  }
+  lastVal = val;
+}
+void handleButtons() {
+  for (int i = 0; i < 5; i++)
+  {
+    currentButtonState[i] = digitalRead(buttonPin[i]); //save current pin state
+    if (currentButtonState[i] != lastButtonState[i]) //check if pin state has changed from last state
     {
-      digitalWrite(LEDPIN, HIGH);
-      //MIDI.sendNoteOn(currentNote, 100, i);
-    } else {
-      digitalWrite(LEDPIN, LOW);
-      //MIDI.sendNoteOff(currentNote, 100, i);
+      if (currentButtonState[i] == HIGH)  //is the button pressed?
+      {
+        MIDImessage(10010000, buttonNote[i], 100);
+      }
+      if (currentButtonState[i] == LOW)  //is the button not pressed?
+      {
+        MIDImessage(10000000, buttonNote[i], 0);
+      }
     }
+    lastButtonState[i] = currentButtonState[i];
   }
 }
+
